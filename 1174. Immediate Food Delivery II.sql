27ef7c7b4fbd4be67c734698c9a54eab22bef9a1
Siam <baust.cse.200201018@gmail.com>
2024-04-03 16:06:18 +0600

    1174 completed

---
+++ 1174. Immediate Food Delivery II.sql
@@ -0,0 +1,14 @@
+
+-- Single row reutnr then WHERE dpt = ()
+-- MULTIPLE row return then use Where (id, date) IN
+
+
+-- Write your MySQL query statement below
+SELECT ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))*100/COUNT(DISTINCT customer_id) ,2) AS immediate_percentage  
+FROM Delivery 
+WHERE (customer_id, order_date) IN
+    ( 
+        SELECT customer_id, MIN(order_date)
+        FROM Delivery 
+        GROUP BY customer_id
+    )

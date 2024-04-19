b5f0f4c8098d4238aded872077f71a69f5762764
Siam <baust.cse.200201018@gmail.com>
2024-04-19 17:23:23 +0600

    1321 Done

---
+++ 1321. Restaurant Growth.sql
@@ -0,0 +1,22 @@
+-- Write your MySQL query statement below
+select c.visited_on,
+(
+    select sum(amount)
+    from Customer
+    where visited_on between date_sub(c.visited_on, interval  6 day ) 
+    and c.visited_on
+) as amount,
+ROUND((
+    select sum(amount)/7
+    from Customer
+    where visited_on between date_sub(c.visited_on, interval  6 day ) 
+    and c.visited_on
+), 2) as average_amount
+
+from Customer c
+where visited_on >= (
+    select date_add(min(visited_on), interval  6 day )
+    from Customer
+)
+group by visited_on
+order by visited_on asc
